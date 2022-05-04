bdc029af2b4ed6b035614adb7807ab682fe0cf59
Plüss Roland <roland@rptd.ch>
2022-05-04 17:10:33 +0200

    prepare example

--- cpp/example/main.cpp
+++ cpp/example/main.cpp
@@ -22,6 +22,14 @@
  * SOFTWARE.
  */
 
+#include <iostream>
+#include <memory.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <sstream>
+
 #include <denetwork/denPool.h>
 #include <denetwork/denRealMessage.h>
 #include <denetwork/value/denValueInteger.h>
@@ -33,7 +41,162 @@
 #include <denetwork/message/denMessageReader.h>
 #include <denetwork/state/denState.h>
 
+void clearScreen(){
+	std::cout << "\x1B[2J";
+}
+
+void screenTopLeft(std::ostream &s){
+	s << "\x1B[H";
+}
+
+enum class Color{
+	black = 0,
+	red,
+	green,
+	yellow,
+	blue,
+	magenta,
+	cyan,
+	white
+};
+
+enum class Style{
+	regular,
+	bold,
+	low,
+	italic,
+	underline,
+	blinking,
+	reverse,
+	background,
+	invisible
+};
+
+void resetColors(std::ostream &s){
+	s << "\033[0m";
+}
+
+void foregroundColor(std::ostream &s, Color color, Style style){
+	s << "\033[" << (int)style << ";" << 30 + (int)color << "m";
+}
+
+void backgroundColor(std::ostream &s, Color color, Style style){
+	s << "\033[" << (int)style << ";" << 40 + (int)color << "m";
+}
+
+void printBar(std::ostream &s, const std::string &title, int length, Color color){
+	const int tlen = title.length();
+	int i;
+	
+	resetColors(s);
+	foregroundColor(s, Color::white, Style::bold);
+	for(i=0; i<14-tlen; i++){
+		s << ' ';
+	}
+	s << title << " [";
+	
+	resetColors(s);
+	foregroundColor(s, color, Style::bold);
+	for(i=0; i<length; i++){
+		s << "\u2593";
+	}
+	
+	resetColors(s);
+	foregroundColor(s, color, Style::low);
+	for(i=length; i<60; i++){
+		s << "\u2591";
+	}
+	
+	resetColors(s);
+	foregroundColor(s, Color::white, Style::bold);
+	s << "]" << std::endl;
+}
+
+void nonblock(bool enable){
+	struct termios ttystate;
+	tcgetattr(STDIN_FILENO, &ttystate);
+	
+	if(enable){
+		ttystate.c_lflag &= ~ICANON;
+		ttystate.c_lflag &= ~ECHO;
+		ttystate.c_cc[VMIN] = 1;
+		std::cout << "\033[?25l";
+		
+	}else{
+		ttystate.c_lflag |= ICANON;
+		ttystate.c_lflag |= ECHO;
+		std::cout << "\033[?25h";
+	}
+	
+	tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+}
+
+bool kbhit(int timeoutms = 0){
+	struct timeval tv;
+	fd_set fds;
+	tv.tv_sec = 0;
+	tv.tv_usec = (__suseconds_t)timeoutms * 1000;
+	FD_ZERO(&fds);
+	FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+	select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+	return FD_ISSET(STDIN_FILENO, &fds);
+}
+
 void dummy(){
+	int bar1 = 12;
+	int bar2 = 56;
+	
+	clearScreen();
+	
+	nonblock(true);
+	
+	bool quit = false;
+	while(!quit){
+		if(kbhit()){
+			const int c = fgetc(stdin);
+			switch(c){
+			case 'q':
+				quit = true;
+				break;
+				
+			case 'w':
+				if(bar1 > 0) bar1--;
+				break;
+				
+			case 'e':
+				if(bar1 < 60) bar1++;
+				break;
+				
+			case '\033':
+				fgetc(stdin); // '['
+				switch(fgetc(stdin)){
+				case 'A': //up arrow
+					if(bar2 < 60) bar2++;
+					break;
+					
+				case 'B': // down arrow
+					if(bar2 > 0) bar2--;
+					break;
+					
+				case 'C': // right arrow
+					break;
+					
+				case 'D': // left arrow
+					break;
+				}
+			}
+		}
+		
+		std::stringstream s;
+		screenTopLeft(s);
+		printBar(s, "Value 1", bar1, Color::red);
+		printBar(s, "Value 2", bar2, Color::green);
+		printBar(s, "Value 3", 10, Color::blue);
+		resetColors(s);
+		std::cout << s.str();
+	}
+	nonblock(false);
+	
 	denState::Ref state(std::make_shared<denState>(false));
 	
 	denValueInt::Ref test(std::make_shared<denValueInt>(denValueIntegerFormat::uint32));
